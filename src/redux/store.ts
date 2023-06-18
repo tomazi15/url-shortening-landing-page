f5259b06654d0468d0bdc084839129a6bf4fc456
Tomazi <tomazi.dev@gmail.com>
2023-06-18 18:34:34 +0100

    feat(🎉): setting up basic structure

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,5 +1,5 @@
-import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
+import { configureStore, ThunkAction, Action } from "@reduxjs/toolkit";
+import counterReducer from "../redux/features/counter/counterSlice";
 
 export const store = configureStore({
   reducer: {
