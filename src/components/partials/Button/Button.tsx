c0b881d0afa05d50a42aea96cee814bb8d57b954
Tomazi <tomazi.dev@gmail.com>
2023-06-21 23:07:16 +0100

    feat(✨): add test for button component

--- src/components/partials/Button/Button.tsx
+++ src/components/partials/Button/Button.tsx
@@ -3,7 +3,7 @@ import style from "./Button.module.scss";
 import { ButtonType } from "../../../types";
 
 const Button: FC<ButtonType> = ({ name }): JSX.Element => (
-  <button className={style.button}>{name}</button>
+  <button className={style.buttonRound}>{name}</button>
 );
 
 export default Button;
